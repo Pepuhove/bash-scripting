97801205d5cc786ac4cc1497ccfd1869f13bb402
Pepuhove <pepshove@gmail.com>
2025-09-04 16:04:22 +0000

    input from user

---
+++ script5.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "welcome to input scripts"
+
+echo
+
+read -p "Enter your username: " name
+sleep 3
+echo "Hi $name"
+echo
+read -sp "Enter your password: " password
+
+echo
+
+echo "logged in successfully"
